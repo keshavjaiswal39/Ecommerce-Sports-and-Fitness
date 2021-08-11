0fbc9b370b683d34c92dd19b747f6cbfa9f5f1ba
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-08-12 01:14:33 +0530

    Responsive

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { GlobalState } from '../../GloalState'
 import Menu from './icon/menu.svg'
 import Cart from './icon/cart.svg'
@@ -11,6 +11,7 @@ function Header() {
     const [isLogged] = state.userAPI.isLogged
     const [isAdmin] = state.userAPI.isAdmin
     const [cart] = state.userAPI.cart
+    const [menu, setMenu] = useState(false)
 
     const logoutUser = async () => {
         await axios.get('/user/logout')
@@ -36,10 +37,16 @@ function Header() {
         )
     }
 
+    const toogleMenu = () => setMenu(!menu)
+
+    const styleMenu = {
+        left: menu ? 0 : "-100%"
+    }
+
 
     return (
         <header>
-            <div className="menu">
+            <div className="menu" onClick={() => setMenu(!menu)}>
                 <img src={Menu} alt="" width="30"/>
             </div>
 
@@ -49,7 +56,7 @@ function Header() {
                 </h1>
             </div>
 
-            <ul>
+            <ul style={styleMenu}>
                 <li>
                     <Link to="/">{isAdmin ? 'Products' : 'Shop'}</Link>
                 </li>
@@ -59,7 +66,7 @@ function Header() {
                     isLogged ? loggedRouter() : <li><Link to="/login">Login 🔶 Register</Link></li>
                 }
 
-                <li>
+                <li onClick={() => setMenu(!menu)}>
                     <img src={Close} alt="" width="30" className="menu"/>
                 </li>
             </ul>
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
-
+const path = require('path')
 
 const app = express()
 app.use(express.json())
@@ -34,7 +34,12 @@ mongoose.connect(URI, {
     console.log('Connected to MongodB')
 })
 
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> {
